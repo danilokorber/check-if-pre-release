9ba0226a3d805a4c5d5e88f0950ea6ace0cb182a
Danilo Koerber <danilo.koerber@bmwgroup.com>
2024-03-18 15:52:02 +0100

    dummy test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,19 +1,30 @@
 import * as core from '@actions/core';
 import { describe, expect, it } from '@jest/globals';
+import nock from 'nock';
 
 jest.mock('@actions/core');
 
+function mockInputs(inputs: { [key: string]: string }) {
+  jest.mocked(core.getInput).mockImplementation((s) => inputs[s] || '');
+}
+
 describe('index', () => {
   beforeEach(() => {
-    const token = core.getInput('github-token');
-    core.info(token);
-    jest.mock('@actions/core');
+    // Mock the inputs
+    mockInputs({
+      'github-token': 'test-token',
+    });
+
+    // Mock the API request
+    nock('https://api.github.com').get('/repos/owner/repo/releases/1').reply(200, {
+      id: 1,
+      tag_name: 'v1.0.0',
+      prerelease: false,
+    });
   });
 
   it('calls run when imported', async () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('../src/index');
-
+    // Add your assertions here
     expect({}).toEqual({});
   });
 });
