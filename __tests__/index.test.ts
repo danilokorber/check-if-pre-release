5d3114e09d37be62b03a1c2cf0d71aaa1c5461d5
Danilo Koerber <danilo.koerber@bmwgroup.com>
2024-03-18 15:26:02 +0100

    Update test

---
+++ __tests__/index.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it, jest } from '@jest/globals';
+import * as main from '../src/main';
+
+// Mock the action's entrypoint
+const runMock = jest.spyOn(main, 'run').mockImplementation(() => Promise.resolve());
+
+describe('index', () => {
+  it('calls run when imported', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    require('../src/index');
+
+    expect(runMock).toHaveBeenCalled();
+  });
+});
