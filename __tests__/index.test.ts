4e3c0fc1b8ac4a0cd256535ec50fb4bd5fb0b08b
Danilo Koerber <danilo.koerber@bmwgroup.com>
2024-03-18 15:32:59 +0100

    Fix tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,6 +1,14 @@
+import * as core from '@actions/core';
 import { describe, expect, it } from '@jest/globals';
 
+jest.mock('@actions/core');
+
 describe('index', () => {
+  beforeEach(() => {
+    const token = core.getInput('github-token');
+    jest.mock('@actions/core');
+  });
+
   it('calls run when imported', async () => {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     require('../src/index');
